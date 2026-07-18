74b6accec779acabd49d0987f93383f8ec3ffae0
Yuvraj Saini <sainiyuvraj672@gmail.com>
2026-07-18 14:42:59 +0530

    Update 1firstprg.py

--- 1firstprg.py
+++ 1firstprg.py
@@ -1,3 +1,4 @@
 #my first python program
 print("i like pizza!")
 print("it's really good")
+print("hello Yuvvzie this is 2nd version ive done the changes from the git repo to test my jenkins automates it or not")
